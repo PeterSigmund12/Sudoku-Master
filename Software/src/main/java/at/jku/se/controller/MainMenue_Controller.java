be4bf995d68aff1474490bd229e2eceaa2352a45
PeterSigmund12 <sigmund.peter1@gmx.at>
2021-04-01 13:32:08 +0200

    SDK 15
    Verlinkung von Hauptmenü zu LoadGame hergestellt

--- Software/src/main/java/at/jku/se/controller/MainMenue_Controller.java
+++ Software/src/main/java/at/jku/se/controller/MainMenue_Controller.java
@@ -4,14 +4,20 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
-public class MainMenue_Controller {
+public class MainMenue_Controller implements Initializable {
 
 
     @FXML
@@ -28,25 +34,36 @@ public class MainMenue_Controller {
 
     @FXML
     public void handleButton_LoadGame(ActionEvent event) throws IOException {
-        Platform.exit();
-        /*Stage oldStage = (Stage)root.getScene().getWindow();;
+        //Platform.exit();
+        Node node = (Node) event.getSource();
+      //  Platform.runLater();
+        Stage oldStage = (Stage)node.getScene().getWindow();;
+
 
         FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("fxml/loadGame.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 450, 400);
-        //Parent root2 = (Parent) fxmlLoader.load();
-
+        fxmlLoader.setLocation(getClass().getResource("/fxml/loadGame.fxml"));
+        Parent root2 = null;
+        try {
+            root2 = (Parent) fxmlLoader.load();
+        } catch (IOException ex) {
+          //  Logger.getLogger(Mainmenu_Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
         Stage stage = new Stage();
         stage.setTitle("Load game");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root2));
 
         stage.show();
-        //oldStage.close();*/
+        oldStage.close();
     }
 
 
     @FXML
     public void handleButton_Exit(ActionEvent event){
         Platform.exit();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
