a52222273fe10a89da4de5d512146cda7b1641fc
VinzenzA <39127819+VinzenzA@users.noreply.github.com>
2021-05-09 23:17:16 +0200

    SDK-45 backend for new user interface part 1, displaying of key values, starting on new points calculator

--- Software/src/main/java/at/jku/se/controller/HighScoreBoardController.java
+++ Software/src/main/java/at/jku/se/controller/HighScoreBoardController.java
@@ -13,6 +13,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -35,14 +37,32 @@ public class HighScoreBoardController implements Initializable {
 
     String selected;
 
+
+    static final int SCORE_LEICHT = 3000;
+    static final int SCORE_MITTEL = 6000;
+    static final int SCORE_SCHWER = 9000;
+    static final int POINTS_MINUS_TIME = 5;
+    static final int POINTS_MINUS_CLICKS = 10;
+    static final int POINTS_MINUS_HINTS = 250;
+
     @FXML
     private Label lbGameName;
     @FXML
-    private Label lbGenerate;
-    @FXML
-    private Label lbDifficulty;
+    private Label lbPlayer;
     @FXML
     private Label lbVersion;
+    @FXML
+    private Label lbGen;
+    @FXML
+    private Label lbScore;
+
+    @FXML
+    private Label lbDiff;
+    @FXML
+    private Label lbTime;
+
+    @FXML
+    private ImageView ivSavegame;
 
 
 
@@ -54,6 +74,52 @@ public class HighScoreBoardController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         fillListView();
+    }
+
+    /**
+     *
+     *
+     * @param time Conveys how much time the player has spend on the Game
+     * @param clicks Contains how many moves the player has made
+     * @param hints Contains how many hints the player has used
+     * @param difficulty Contains the difficulty level of the game
+     * @return
+     */
+    public int calculateScore( int time, int clicks, int hints, String difficulty) {
+        int availablePoints;
+
+        //Points for each difficulty
+        switch(difficulty)
+        {
+            case "leicht":
+                System.out.println("leicht");
+              availablePoints =  SCORE_LEICHT;
+                break;
+            case "mittel":
+                System.out.println("mittel");
+                availablePoints =  SCORE_MITTEL;
+                break;
+            case "schwer":
+                System.out.println("schwer");
+                availablePoints =  SCORE_SCHWER;
+                break;
+            default:
+                System.out.println("no match");
+                availablePoints = 0;
+        }
+
+        availablePoints = availablePoints - clicks* POINTS_MINUS_CLICKS;
+
+        //TODO -Zeit
+        availablePoints = availablePoints - time* POINTS_MINUS_TIME;
+
+        availablePoints = availablePoints - hints* POINTS_MINUS_HINTS;
+
+        // TODO verify if ok
+        if (availablePoints < 0) {
+            availablePoints = 0;
+        }
+        return availablePoints;
     }
 
     public void fillListView() {
@@ -77,7 +143,6 @@ public class HighScoreBoardController implements Initializable {
                 System.out.println(newValue);
 
                 try {
-
                     // get values string and get points,
                     //read in the current document
                     Object obj = jsonparser.parse(new FileReader("./savegames/JSON/" + newValue + ".json"));
@@ -87,13 +152,26 @@ public class HighScoreBoardController implements Initializable {
                     String name = (String) gameinfos.get("FileName");
 
                     //make sure strring exists
-                    String points = "0";
-
+                    /*String points = "0";
                     points = (String) gameinfos.get("Score");
 
                     int pointsInt = Optional.ofNullable(points)
                             .map(Ints::tryParse)
                             .orElse(0);
+                     */
+                    //FIXME
+                    // -Remove points
+                    // -Sicherstellen, dass alle werte existieren
+                    // -Aufruf Methode
+
+                    int time;
+                     int clicks;
+                     int hints;
+                     String difficulty;
+
+                    int pointsInt=0;
+
+
 
                     //create a new HighScore Object to store value
                     HighScoreObject hSObject = new HighScoreObject(pointsInt, name);
@@ -112,7 +190,7 @@ public class HighScoreBoardController implements Initializable {
         Collections.sort(higScoreList, HighScoreObject.aHighScore);
         for (HighScoreObject str : higScoreList) {
             System.out.println(str.getHighScore());
-            highScoreGameList.add(str.getGameName() + ": " + str.getHighScore());
+            highScoreGameList.add(str.getGameName() ); //+ ": " + str.getHighScore());
         }
 
 
@@ -120,6 +198,52 @@ public class HighScoreBoardController implements Initializable {
         lvHighScoreGames.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+               // brauche ich herzlich wenig btnContinue.setDisable(false);
+                selected = newValue;
+                System.out.println(newValue);
+
+                try {
+                    Object obj =   jsonparser.parse(new FileReader("./savegames/JSON/"+newValue+".json"));
+                    JSONObject gameinfos = (JSONObject) obj;
+                    String name = (String)gameinfos.get("FileName");
+
+                    HighScoreObject customer;
+                            HighScoreObject test = higScoreList.stream()
+                            .filter(HighScoreObject -> name.equals(HighScoreObject.getGameName()))
+                            .findAny()
+                            .orElse(null);
+
+
+                    System.out.println(test.getGameName());
+
+
+                   File imgfile = new File("./savegames/img/"+name+".png");
+                   Image image = new Image(imgfile.toURI().toString());
+                   // lbGameName.setText(name);
+                    lbVersion.setText((String)gameinfos.get("version"));
+
+                    //TODO search in liste for namen und rufe dann in der liste den score auf.
+
+                    lbScore.setText((String)gameinfos.get("Score"));
+                    lbDiff.setText((String)gameinfos.get("difficulty"));
+                    lbTime.setText((String)gameinfos.get("Zeit"));
+                    lbGen.setText((String)gameinfos.get("generateType"));
+                    lbPlayer.setText((String)gameinfos.get("Player"));
+
+                    //lbDiff.setText((String)gameinfos.get("difficulty"));
+                    ivSavegame.setImage(image);
+
+                } catch (IOException|ParseException e) {
+                    e.printStackTrace();
+                }
+
+
+
+
+
+
+
+
 
 
                 selected = newValue;
